4482d32379ad681ab499737c623cf660bbaeaf8b
Phan Nguyễn Hoàng Phương <pnhphuong.work1@gmail.com>
2024-03-26 14:56:41 +0700

    Add services upgradeModule, getAllModules and add comments

--- src/api/services/odooService/odooModuleService.js
+++ src/api/services/odooService/odooModuleService.js
@@ -1,12 +1,22 @@
-const { getCompanyByDbName, isExistUserByDbName} = require('../companyService');
+const {isExistCompanyByDbName} = require('../companyService');
 const axios = require('../../../utils/axiosUtil');
 //các hàm liên quan module
 //lay DB name -> get cai company -> sosanh userid == user hien tai
 
+/**
+ * Activate Module
+ * @param userId
+ * @param master_pwd
+ * @param dbname
+ * @param lang
+ * @param password
+ * @param moduleId
+ * @returns {Promise<{data: axios.AxiosResponse<any>, message: string, isSuccess: boolean}>}
+ */
 async function activeOdooModule(userId, master_pwd, dbname,
                                 lang, password, moduleId) {
 
-    const company = await isExistUserByDbName(dbname);
+    const company = await isExistCompanyByDbName(dbname);
 
     const url = `https://${dbname}.${process.env.ROOT_ODOO_DOMAIN}/web/database/modules`;
 
@@ -47,7 +57,7 @@ async function activeOdooModule(userId, master_pwd, dbname,
 async function deactivateModule(userId, master_pwd, dbName,
                                 lang, password, moduleId) {
 
-    const company = await isExistUserByDbName(userId, dbName);
+    const company = await isExistCompanyByDbName(userId, dbName);
 
     const url = `https://${dbName}.${process.env.ROOT_ODOO_DOMAIN}/web/database/modules`;
 
@@ -74,7 +84,75 @@ async function deactivateModule(userId, master_pwd, dbName,
     }
 }
 
+/**
+ * Upgrade module data
+ * @param userId
+ * @param master_pwd
+ * @param dbName
+ * @param lang
+ * @param password
+ * @param moduleId
+ * @returns {Promise<{data: axios.AxiosResponse<any>, message: string}>}
+ */
+async function upgradeModule(userId, master_pwd, dbName, lang, password, moduleId) {
+    const company = await isExistCompanyByDbName(userId, dbName);
+    const url = `https://${dbName}.${process.env.ROOT_ODOO_DOMAIN}/web/database/modules`;
+    try {
+        const data = {
+            master_pwd: master_pwd,
+            dbname: dbName,
+            lang: lang,
+            password: password,
+            moduleId: moduleId
+        };
+
+        const result = await axios.axiosPatch(url, data, {
+            'API_KEY': company.apiKey
+        });
+
+        return {
+            message: "Successfully upgrade module",
+            data: result
+        };
+
+    } catch (error) {
+        throw new Error("Error at deactivate module: " + error.message);
+    }
+}
+
+/**
+ * Get all modules via DBName
+ * @param userId
+ * @param dbName
+ * @param moduleId
+ * @returns {Promise<{data: axios.AxiosResponse<any>, message: string}>}
+ */
+async function getAllModules(userId, dbName, moduleId) {
+    const company = await isExistCompanyByDbName(userId, dbName);
+    const url = `https://${dbName}.${process.env.ROOT_ODOO_DOMAIN}/web/database/modules`;
+
+    const data = {
+        dbname: dbName,
+        moduleId: moduleId
+    }
+    try {
+        const result = await axios.axiosPost(url, data, {
+            'API_KEY': company.apiKey
+        });
+
+        return {
+            message: "Successfully get all modules",
+            data: result
+        };
+
+    } catch (error) {
+        throw new Error("Error at deactivate module: " + error.message);
+    }
+}
+
 module.exports = {
     activeOdooModule,
-    deactivateModule
+    deactivateModule,
+    upgradeModule,
+    getAllModules,
 }
