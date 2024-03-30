831048cf3f2962761671b3b770433789d67259e5
AnhDuc <naduccva@gmail.com>
2024-03-30 16:34:15 +0700

    allow all cors

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -8,13 +8,18 @@ const https_localhost_url = 'https://localhost:3000'
 const allowedOrigins = [http_api_url, https_api_url, https_page_url, http_localhost_url, https_localhost_url]
 
 const corsOptions = {
-    origin: function(origin, callback) {
-        if(!origin || allowedOrigins.includes(origin)){
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by Cors"));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'PATCH' ,'DELETE']
-}
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*'
+};
+// const corsOptions = {
+//     origin: function(origin, callback) {
+//         if(!origin || allowedOrigins.includes(origin)){
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by Cors"));
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'PATCH' ,'DELETE']
+// }
 module.exports = cors(corsOptions);
