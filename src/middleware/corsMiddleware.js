9fafdde0577d8ca8031e22b40b26de6398ac24d1
AnhDuc <naduccva@gmail.com>
2024-03-30 15:06:17 +0700

    add localhost cors

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const http_api_url = 'http://' + process.env.API_URL.toString();
 const https_api_url = 'https://' + process.env.API_URL.toString();
 const https_page_url = process.env.REACT_APP_ROOT_FE.toString();
-const allowedOrigins = [http_api_url, https_api_url, https_page_url]
+const http_localhost_url = 'http://localhost:3000'
+const https_localhost_url = 'https://localhost:3000'
+const allowedOrigins = [http_api_url, https_api_url, https_page_url, http_localhost_url, https_localhost_url]
 
 const corsOptions = {
     origin: function(origin, callback) {
