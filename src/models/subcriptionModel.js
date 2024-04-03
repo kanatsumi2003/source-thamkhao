36adb9213f2d5c8692be6a2a7f86714635ed1a60
kanatsumi2003 <naduccva@gmail.com>
2024-04-03 14:57:22 +0700

    fix model

--- src/models/subcriptionModel.js
+++ src/models/subcriptionModel.js
@@ -4,12 +4,10 @@ const BaseModel = require("./baseModel");
 
 // dbname domainname, startdate, enddate, total invoices, total storage// name, image, description, monthly prices, yearly price, total //sendemail
 class Subcription {
-    constructor(name, startDate, endDate, total_invoices, total_storage, image, description, monthly_prices, yearly_prices, total){
+    constructor(name, total_invoices, total_storage, image, description, monthly_prices, yearly_prices, total){
         this.name = name,
         this.dbName = dbName,
         this.domainName = domainName,
-        this.startDate = startDate,
-        this.endDate = endDate,
         this.total_invoices = total_invoices,
         this.total_storage = total_storage,
         this.image = image,
